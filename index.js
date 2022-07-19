aa59adf7a7b2351e4e5d6eed77d1b350ff57f42b
Acanixz <professional.herickvvb@gmail.com>
2022-07-19 18:10:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,49 +3,7 @@ All rights reserved.
 
 This source code is licensed under the MIT-style license found in the
 LICENSE file in the root directory of this source tree. 
-
-    SETUP INSTRUCTIONS 
-    Running locally:
-    1 - Create MiniScraperConfig.json
-    Paste the code below there and ajust the settings:
-
-    {
-    "timeOffset": 0,
-    "URL": "",
-    "mutationTarget": "",
-    "extractionTarget": "",
-    "fileName": "./data/lastExtraction.json"
-    }
-
-    Running on a server (Using Heroku as a base for this):
-    
-    1 - Create the following environment variables in your project's settings:
-    SCPR_timeOffset (this is the main one, if this one does not exist, it will run locally)
-    SCPR_URL
-    SCPR_mutationTarget
-    SCPR_extractionTarget
-    SCPR_Filename
-
-    2 - Install heroku's native nodejs buildpack
-    https://elements.heroku.com/buildpacks/heroku/heroku-buildpack-nodejs
-
-    3 - Install the puppeteer buildpack
-    https://elements.heroku.com/buildpacks/jontewks/puppeteer-heroku-buildpack
-
-    Notes:
-    - timeOffset is used to ajust the hours in servers that you cannot change timezone
-
-    - timeOffset should always be 0 when running locally (since time is adquired
-    from your computer's clock, it should always be correct)
-
-    - mutationTarget is usually a div that the program will watch for child changes
-
-    - extractionTarget is the child from the mutation, you can have multiple classes
-
-    - fileName should be kept as it is, unless you want to change the source code to support
-    multiple values/ change path/ change name.
 */
-
 
 "use strict";
 const puppeteer = require('puppeteer')
